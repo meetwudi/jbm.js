e1f44aacf80ae5cbd8a7a6bc3f754c2cde5ee714
John Wu <webmaster@leapoahead.com>
2014-04-06 12:56:38 +0800

    add test related stuff

---
+++ test/spec/helper.spec.js
@@ -0,0 +1,23 @@
+define(['should', 'jbm'], function(should, jbm) {
+
+  describe('helper test', function() {
+    it('parse function parameter name list', function(done) {
+      var argNames = jbm.debug.parseParams(function(a, b, $test){});
+      should.deepEqual(argNames, ['a', 'b', '$test']);
+      done();
+    });
+
+    it('can check if an function requires a done function', function(done) {
+      var result;
+      result = jbm.debug.verifyAsync(function(done) {});
+      should(result).be.ok;
+      result = jbm.debug.verifyAsync(function() {});
+      should(result).not.be.ok;
+      result = jbm.debug.verifyAsync(function(arb) {});
+      should(result).not.be.ok;
+      done();
+    });
+  });
+
+  return true;
+});

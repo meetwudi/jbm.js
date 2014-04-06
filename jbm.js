f73c01d7f221657a4af3ab05839fe8a0a327a6fa
John Wu <webmaster@leapoahead.com>
2014-04-06 10:51:03 +0800

    early implementation

--- jbm.js
+++ jbm.js
@@ -1,11 +1,95 @@
 (function(window) {
   "use strict";
-  var jbm, _originJbm;
+  var jbm, 
+      _originJbm,
+      debugMode = !!window.__karma__; 
 
+  /**
+   *  Helper methods
+   */
+  
+  // get parameter list of fn
+  function parseParams(fn) {
+    var fns = fn.toString(),
+        names = fns.slice(fns.indexOf('(') + 1, fns.indexOf(')')).split(','),
+        i;
+    for (i = 0; i < names.length; i ++) {
+      names[i] = names[i].replace(/\s+/, '');
+    }
+  }
+
+  // test if a string is in the array
+  function contains(value, arr) {
+    var i;
+    for (i = 0; i < arr.length; i ++) {
+      if (arr[i] === value) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // check if fn requires a done function
+  function verifyAsync(fn) {
+    var params = parseParams(fn);
+    return contains('done', params);
+  }
+
+
+
+
+  /**
+   *  Jbm API
+   */
+  
+  // test case wrapper
+  jbm.runTest = function(name, fn, reporterCallback) {
+    jbm.runTest(name, fn, function(result) {
+
+    });
+  };
+  
   // Restore window.jbm to its original state before jbm initialization
   jbm.noConflict = function() {
     window.jbm = _originJbm;
     return jbm;
+  };
+
+  /**
+   *  Jbm private methods
+   */
+  
+  // main body of test runner
+  // pass the timeUsed into cb callback
+  jbm._runTest = function(name, fn, cb) {
+    var isAsync = verifyAsync(fn),
+        ts = performance.now();
+    if (isAsync) {
+      fn(jbm._done.bind(this));
+    }
+    else {
+      fn();
+      cb(performance.now() - ts);
+    }
+  }
+
+  // done function called when async test case finishes
+  // This function's context is binded to jbm._runTest
+  jbm._done = function() {
+    cb(performance.now() - ts);
+  }
+
+
+  /**
+   *  Initialization
+   */
+  
+  // addtional API for debugging only
+  if (debugMode) {
+    jbm.debug = {};
+    jbm.debug.parseParams = parseParams;
+    jbm.debug.contains = contains;
+    jbm.debug.verifyAsync = verifyAsync;
   }
 
   // Set window.jbm to jbm itself.
@@ -19,4 +103,5 @@
       return jbm.noConflict();
     });
   }
+
 })(window);
